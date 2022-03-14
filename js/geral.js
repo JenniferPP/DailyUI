e105afff86d36dfd2e09b47ed28046dcd683185c
JenniferPP <jenni-pp@hotmail.com>
2022-03-14 15:02:31 -0300

    Subindo arquivos no Github

---
+++ js/geral.js
@@ -0,0 +1,8 @@
+function versenha(obj){
+    var obj = document.getElementById('password');
+    obj.type = "text";
+}
+function ocultarsenha(obj){
+    var obj = document.getElementById('password');
+    obj.type = "password"
+}
